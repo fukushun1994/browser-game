8d6d6153c759bec96af666ed8ddbf5d15c11be7d
Shunya Fukunaga <fukushun1994@gmail.com>
2019-03-18 01:24:14 +0900

    ヘッダの追加

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Button from '@material-ui/core/Button';
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
-      <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+      <Header />
     );
   }
 }

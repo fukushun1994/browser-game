05d287950b713a5d4f9023a4240ea581e91a1bc9
Shunya Fukunaga <fukushun1994@gmail.com>
2019-03-18 00:46:37 +0900

    material-uiの導入

---
+++ client/react-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    );
+  }
+}
+
+export default App;
